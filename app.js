2a62b48a70592745c782b2a7830884f1635a15ac
Miyka96 <micaelaseminara@outlook.it>
2022-02-07 20:53:29 +0100

    firsts files and firsts js elements

---
+++ app.js
@@ -0,0 +1,8 @@
+// Creo una variabile per il container
+const contenitore = document.getElementById ("container");
+
+// Genero stampa dei numeri da 1 a 100
+
+for ( let i=1; i<101 ; i++) {
+    
+}
