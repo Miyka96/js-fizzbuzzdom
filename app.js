8502fb16618172de42c7104f6c6e7d0df25d95eb
Miyka96 <micaelaseminara@outlook.it>
2022-02-07 22:23:03 +0100

    progress (css style)

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-const container = document.querySelector('.container');
+const list = document.getElementById('list');
 
 // Genero stampa dei numeri da 1 a 100
 
 for ( let i = 1; i < 101 ; i++) {
+    console.log (i);
 
-    const squares = document.createElement('div');
+    const squares = document.createElement('li');
     squares.classList.add('square-wrapper');
-    container.append('squares');
+    list.append(squares);
 
-    squares.append(i);
+    if (i %15 ===0){
+        squares.classList.add('fizz-buzz-box');
+        squares.append('fizzbuzz');
+    }
+    else if  (i %3 === 0) {
+        squares.classList.add('fizz-box');
+        squares.append('fizz')    
+    }
+    else if (i %5 ===0){
+        squares.classList.add('buzz-box');
+        squares.append('buzz');
+    }
+    else{
+        squares.append(i);
+    }
 }
+
+
