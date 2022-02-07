6d9ea3993faa6df95fdd7bc65b57713dfcdce8ea
Miyka96 <micaelaseminara@outlook.it>
2022-02-07 21:16:14 +0100

    some progress?!

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-// Creo una variabile per il container
-const contenitore = document.getElementById ("container");
+const container = document.querySelector('.container');
 
 // Genero stampa dei numeri da 1 a 100
 
-for ( let i=1; i<101 ; i++) {
-    
+for ( let i = 1; i < 101 ; i++) {
+
+    const squares = document.createElement('div');
+    squares.classList.add('square-wrapper');
+    container.append('squares');
+
+    squares.append(i);
 }
